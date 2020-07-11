097e7a4dead98442d6e32a36aa68375459da3bd2
Dorin Popa <dorin.popa@ellation.com>
2020-07-11 12:47:46 +0300

    Updates

---
+++ precache-manifest.fdac7aaa9a2345dce6fd1278ddf035b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f02fd38f46ca85c1279144f1908fdb3",
+    "url": "/pwa-react/index.html"
+  },
+  {
+    "revision": "2828178e34c0cb31f1c5",
+    "url": "/pwa-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f3791d0e8b80d17e1be0",
+    "url": "/pwa-react/static/js/2.7818b5a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pwa-react/static/js/2.7818b5a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2828178e34c0cb31f1c5",
+    "url": "/pwa-react/static/js/main.0a327f94.chunk.js"
+  },
+  {
+    "revision": "27b2f5d285eede33f3d9",
+    "url": "/pwa-react/static/js/runtime-main.c04c1cdd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pwa-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
